0336df46f7c2840df2abf12e207dd203de8f0efe
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-02-13 16:41:07 +0100

    Create User, Consultant, Project class
    Create User, Consultant, Project class
    
    Refacto of Client and consultant which are extends of a generic User class

--- domain/model/Client.java
+++ domain/model/Client.java
@@ -1,53 +1,23 @@
 package com.architrademe.domain.model;
 
-import java.util.Objects;
+public class Client extends User {
 
-public class Client {
-    private final long uid = 1L;
-    private String name;
-    private String surname;
-    private String email;
-    private String phone;
+    public Client() {}
 
-    public Client(String name, String surname, String email, String phone) {
-        this.uid = ++uid; //To automatically generate new UID on Client creation
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
+    public Client(String name, String surname, String phone, String email, String password) {
+        super(name, surname, phone, email, password);
     }
 
-    public long getUid() {
-        return uid;
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + super.getId() +
+                ", name='" + super.getName() + '\'' +
+                ", surname='" + surname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + super.getEmail() + '\'' +
+                ", projects=" + super.getProjects() +
+                ", password='" + super.getPassword() + '\'
+                '}';
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
-        Client client = (Client) object;
-        return uid == client.uid && java.util.Objects.equals(name, client.name) && java.util.Objects.equals(surname, client.surname) && java.util.Objects.equals(email, client.email) && java.util.Objects.equals(phone, client.phone);
-    }
-
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), uid, name, surname, email, phone);
-    }
-
-
 }
---
+++ domain/model/Consultant.java
@@ -0,0 +1,69 @@
+package com.architrademe.domain.model;
+
+import java.util.List;
+
+public class Consultant extends User {
+    private List<String> skills;
+    private Double hourlyRate;
+    private Boolean available;
+    private String paymentModality;
+
+    public Consultant() {}
+
+    public Consultant(String name, String surname, String phone, String email, String password, List<String> skills, Double hourlyRate, Boolean available, String paymentModality) {
+        super(name, surname, phone, email, password);
+        this.skills = skills;
+        this.hourlyRate = hourlyRate;
+        this.available = available;
+        this.paymentModality = paymentModality;
+    }
+
+    public List<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<String> skills) {
+        this.skills = skills;
+    }
+
+    public Double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(Double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public Boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public String getPaymentModality() {
+        return paymentModality;
+    }
+
+    public void setPaymentModality(String paymentModality) {
+        this.paymentModality = paymentModality;
+    }
+
+    @Override
+    public String toString() {
+        return "Consultant{" +
+                "id=" + super.getId() +
+                ", name='" + super.getName() + '\'' +
+                ", surname='" + super.getSurname() + '\'' +
+                ", email='" + super.getEmail() + '\'' +
+                ", phone='" + super.getPhone() + '\'' +
+                ", skills=" + skills +
+                ", hourlyRate=" + hourlyRate +
+                ", available=" + available +
+                ", paymentModality='" + paymentModality + '\'' +
+                ", projects=" + super.getProjects() +
+                ", password='" + super.getPassword() + '\'' +
+                '}';
+    }
+}
