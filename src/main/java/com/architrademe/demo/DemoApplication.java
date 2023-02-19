fd9e1c0928a34172df1418e9d344d6f569824aaf
Driss Kanouni <32987211+GetMetamorph@users.noreply.github.com>
2023-02-19 18:15:21 +0100

    add quickstart spring root

--- src/main/java/com/architrademe/demo/DemoApplication.java
+++ src/main/java/com/architrademe/demo/DemoApplication.java
@@ -1,19 +1,25 @@
 package com.architrademe.demo;
 
-import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.architrademe.domain.repository")
+@EnableJpaRepositories(basePackages = "infrastructure.persistence.repository")
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
-
